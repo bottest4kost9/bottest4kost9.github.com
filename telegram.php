57da4f3a72f204250725afc505da056efa796a7d
bottest4kost9 <85102752+bottest4kost9@users.noreply.github.com>
2021-05-31 11:38:22 +0300

    add

---
+++ telegram.php
@@ -0,0 +1,23 @@
+<?php
+
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+$msg = $_POST['msg'];
+$token = "1007513873:AAHRuGZ8Cr1wULSsMevQi6S1KtbdwbSyfiU";
+$chat_id = "-502850019";
+$arr = array(
+  'Имя пользователя: ' => $name,
+  'Телефон: ' => $phone,
+  'Email' => $email,
+  'Сообщение:' => $msg
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+
+?>

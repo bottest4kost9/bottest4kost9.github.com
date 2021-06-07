8e9340c24153031265d1e1434f2433b0db081446
bottest4kost9 <85102752+bottest4kost9@users.noreply.github.com>
2021-06-07 22:29:52 +0300

    nadpod

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
+$(document).ready(function () {
+
+	$('a[href^="#"]').click(function () {
+		//Сохраняем значение атрибута href в переменной:
+		var target = $(this).attr('href');
+		$('html, body').animate({
+			scrollTop: $(target).offset().top//можно вычесть высоту меню
+		}, 700);
+		return false;
+	});
+
+});
+
+
 let elem = document.getElementById('arrow')
 window.onscroll = function showArrow(){
     if(window.pageYOffset >300){
